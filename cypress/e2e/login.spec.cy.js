fa857a02c645e8786eae3c502d52a1dbfabceb3f
Leandro Reis <leohbr@gmail.com>
2024-04-30 14:52:39 -0300

    config selectorList object

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,22 +1,40 @@
 /// <reference types="cypress" />
 
+import user from '../fixtures/users/user.json'
+
 describe('Orange-HRM Tests', () => {
+
+    const selectorList = {
+        usernameField: "[name='username']",
+        passwordField: "[name='password']",
+        submitButton: "[type='submit']"
+    }
+
     it('Success login', () => {
         cy.visit('/auth/login')
         cy.title().should('eq', 'OrangeHRM')
-        cy.get("[name='username']").type('Admin')
-        cy.get("[name='password']").type('admin123')
-        cy.get("[type='submit']").click()
+        cy.get(selectorList.usernameField).type(user.name)
+        cy.get(selectorList.passwordField).type(user.password)
+        cy.get(selectorList.submitButton).click()
         cy.location('pathname').should('eq', '/web/index.php/dashboard/index')
         cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('have.text', 'Dashboard')
     })
 
-    it('Failed login', () => {
+    it('Failed login with invalid user', () => {
         cy.visit('/auth/login')
         cy.title().should('eq', 'OrangeHRM')
-        cy.get("[name='username']").type('Admin')
-        cy.get("[name='password']").type('admin1234')
-        cy.get("[type='submit']").click()
+        cy.get(selectorList.usernameField).type('Administrator')
+        cy.get(selectorList.passwordField).type(user.password)
+        cy.get(selectorList.submitButton).click()
+        cy.get('.oxd-alert-content').should('have.text', 'Invalid credentials')
+    })
+
+    it('Failed login with invalid password', () => {
+        cy.visit('/auth/login')
+        cy.title().should('eq', 'OrangeHRM')
+        cy.get(selectorList.usernameField).type(user.name)
+        cy.get(selectorList.passwordField).type('admin1234')
+        cy.get(selectorList.submitButton).click()
         cy.get('.oxd-alert-content').should('have.text', 'Invalid credentials')
     })
 })
