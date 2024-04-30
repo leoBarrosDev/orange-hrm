b71133f71412a5600ab0439376ace3dd4dc46456
Leandro Reis <leohbr@gmail.com>
2024-04-30 09:09:35 -0300

    completed login test

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -4,5 +4,9 @@ describe('Orange-HRM Tests', () => {
     it('Success login', () => {
         cy.visit('/auth/login')
         cy.title().should('eq', 'OrangeHRM')
+        cy.get('input[name="username"]').type('Admin')
+        cy.get('input[name="password"]').type('admin123')
+        cy.get('button[type="submit"]').click()
+        cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('have.text', 'Dashboard')
     })
 })
