890de28782f102f813b7bbbd595451c70dedf14d
Leandro Reis <leohbr@gmail.com>
2024-04-30 14:51:15 -0300

    add setTimeout

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://opensource-demo.orangehrmlive.com/web/index.php',
     setupNodeEvents(on, config) {
+      setTimeout: 30000
       // implement node event listeners here
     },
   },
